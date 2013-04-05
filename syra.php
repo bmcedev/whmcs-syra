e8a1ec7bfba269aeb76ef7894386eeb24e5779b5
Melissa Choy <mel@otilas.com.au>
2013-04-06 10:37:35 +1100

    initial commit

---
+++ syra.php
@@ -0,0 +1,11 @@
+<?php
+
+function syra_getConfigArray() {
+	$configarray = array(
+	 "ResellerID" => array( "Type" => "text", "Size" => "20", "Description" => "Enter your reseller id here", ),
+	 "APIKey" => array( "Type" => "password", "Size" => "20", "Description" => "Enter your api key here", ),
+	 "TestMode" => array( "Type" => "yesno", )
+	);
+	return $configarray;
+}
+
