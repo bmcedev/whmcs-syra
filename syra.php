faa764e9023b57f3b9f1bf1919d623fe84122b10
Melissa Choy <mel@otilas.com.au>
2013-04-11 21:50:44 +1000

    include syra api

--- syra.php
+++ syra.php
@@ -1,5 +1,9 @@
 <?php
 
+$dir = realpath(dirname(__FILE__));
+
+include_once "{$dir}/syra/syra.php";
+
 function syra_getConfigArray() {
 	$configarray = array(
 	 "ResellerID" => array( "Type" => "text", "Size" => "20", "Description" => "Enter your reseller id here", ),
